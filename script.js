7ae59402797d8afa3f9302a5772069be3711247b
PHPLaratuananh <36061508+n0xgg04@users.noreply.github.com>
2023-01-05 21:18:22 +0700

    OK
    OK

--- script.js
+++ script.js
@@ -18,29 +18,45 @@ const DelayTime = (sec) => {
 const luu = (monhoc) => { toDKSelectedChange(monhoc) }
 
 //CONFIG ---------------------------------------------------
-const monIdSet = 'chk_BAS115126    '; //In mon
-const tuDongLuu = false; //Tu dong luu
+const tuDongLuu = true;
+const luaChon = ['33', '26', '13', '14', '03']
+var monIdSet = 'chk_BAS115126    ';
+
+function getIDmon(idTiet) {
+    return `chk_BAS1151${idTiet}    `;
+}
 //----------------------------------------------------------
 
-
+var doneTask = 0;
 const task = async () => {
-    switchTo('BAS1224') //Chuyen sang Tab mon Vat ly
-    await DelayTime(3000)
+    switchTo('BAS1224')
+    await DelayTime(1500)
     switchTo('BAS1151')
-    await DelayTime(2000) //Tab chua mon can dangky
-    var selectedSub = document.getElementById(monIdSet)
-    if (!selectedSub.disabled) {
-        selectedSub.checked = 1;
-        luu(selectedSub)
-        await DelayTime(1000)
-        console.log(`%cSuccess! Da dang ky thanh cong !`, 'color: green;');
-        await DelayTime(4000)
-        if(tuDongLuu) LuuDanhSachDangKy()
-    } else {
-        console.log('%cFailed! Chua co slot !', 'color:green')
+    await DelayTime(1500)
+    for (let i = 0; i < luaChon.length; i++) {
+        monIdSet = getIDmon(luaChon[i]);
+        var selectedSub = document.getElementById(monIdSet)
+        try {
+            selectedSub.disabled
+        } catch (err) {
+            selectedSub.disabled = false;
+            console.log(`%cFailed! Chua co slot ${monIdSet}!`, 'color:red')
+        }
+        if (!selectedSub.disabled) {
+            selectedSub.checked = 1;
+            luu(selectedSub)
+            await DelayTime(1000)
+            console.log(`%cSuccess! Da dang ky thanh cong!`, 'color: green;');
+            await DelayTime(1500)
+            if (tuDongLuu) LuuDanhSachDangKy()
+            doneTask = 1;
+            break;
+        } else {
+            console.log(`%cFailed! Chua co slot ${monIdSet}!`, 'color:red')
+        }
     }
 }
 
 const RunAuto = async (delay = 5000) => {
-    while (true) { await task(); await DelayTime(delay); }
+    while (doneTask == 0) { await task(); await DelayTime(delay); }
 }
